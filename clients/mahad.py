a1fd05b484e9e635a0684eba7dd1af8dcb00c0b5
Datavoxx <info@datavoxx.se>
2026-03-21 21:18:17 +0100

    Add client Mahad (slack)

---
+++ clients/mahad.py
@@ -0,0 +1,13 @@
+CLIENT_NAME = "Mahad"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+INTERFACE   = "slack"
+
+MODAL_APP_NAME  = "calendar-mahad"
+MODAL_DICT_NAME = "mahad-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-mahad"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-mahad"
+MODAL_SECRET_OAUTH     = "google-oauth-app"
+MODAL_SECRET_TELEGRAM  = None
