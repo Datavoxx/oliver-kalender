c7af0f1993e11f52949821c643fa0a44c136d753
Datavoxx <info@datavoxx.se>
2026-03-21 21:23:22 +0100

    Add client Oskar (slack)

---
+++ clients/oskar.py
@@ -0,0 +1,13 @@
+CLIENT_NAME = "Oskar"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+INTERFACE   = "slack"
+
+MODAL_APP_NAME  = "calendar-oskar"
+MODAL_DICT_NAME = "oskar-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-oskar"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-oskar"
+MODAL_SECRET_OAUTH     = "google-oauth-app"
+MODAL_SECRET_TELEGRAM  = None
