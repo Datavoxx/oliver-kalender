0f66839f7449f3283bed08c68787e7a934dca84e
Datavoxx <info@datavoxx.se>
2026-03-21 22:46:44 +0100

    Add client Umar (slack)

---
+++ clients/umar.py
@@ -0,0 +1,13 @@
+CLIENT_NAME = "Umar"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+INTERFACE   = "slack"
+
+MODAL_APP_NAME  = "calendar-umar"
+MODAL_DICT_NAME = "umar-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-umar"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-umar"
+MODAL_SECRET_OAUTH     = "google-oauth-app"
+MODAL_SECRET_TELEGRAM  = None
