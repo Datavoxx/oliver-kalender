8c59e6761881622503974a73d095a90ea2f8d67d
Datavoxx <info@datavoxx.se>
2026-03-21 21:02:53 +0100

    Add client Jonas (slack)

---
+++ clients/jonas.py
@@ -0,0 +1,13 @@
+CLIENT_NAME = "Jonas"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+INTERFACE   = "slack"
+
+MODAL_APP_NAME  = "calendar-jonas"
+MODAL_DICT_NAME = "jonas-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-jonas"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-jonas"
+MODAL_SECRET_OAUTH     = "google-oauth-app"
+MODAL_SECRET_TELEGRAM  = None
