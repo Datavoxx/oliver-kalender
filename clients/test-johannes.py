0984c5d814b3f68654ccc71461fb610f8b509f6e
Datavoxx <info@datavoxx.se>
2026-03-26 23:31:09 +0100

    Add test client Johannes

---
+++ clients/test-johannes.py
@@ -0,0 +1,10 @@
+CLIENT_NAME = "Johannes"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+
+MODAL_APP_NAME  = "calendar-test-johannes"
+MODAL_DICT_NAME = "test-johannes-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-test-johannes"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-test-johannes"
