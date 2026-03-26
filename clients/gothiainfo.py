d7dd130874e1c415468fcd63c7c7a610ce095387
Datavoxx <info@datavoxx.se>
2026-03-26 18:52:49 +0100

    Add client Gothiainfo

---
+++ clients/gothiainfo.py
@@ -0,0 +1,10 @@
+CLIENT_NAME = "Gothiainfo"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+
+MODAL_APP_NAME  = "calendar-gothiainfo"
+MODAL_DICT_NAME = "gothiainfo-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-gothiainfo"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-gothiainfo"
