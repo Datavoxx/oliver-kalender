651b7b401870c48450dc439a9b8c31f732e0e933
Datavoxx <info@datavoxx.se>
2026-03-26 19:25:24 +0100

    Add test client [namn]

---
+++ clients/test-[namn].py
@@ -0,0 +1,10 @@
+CLIENT_NAME = "[namn]"
+TIMEZONE    = "Europe/Stockholm"
+LANGUAGE    = "Swedish"
+
+MODAL_APP_NAME  = "calendar-test-[namn]"
+MODAL_DICT_NAME = "test-[namn]-state"
+
+MODAL_SECRET_OPENAI    = "openai-api-key"
+MODAL_SECRET_AUTH      = "api-auth-token-test-[namn]"
+MODAL_SECRET_GOOGLE    = "google-calendar-credentials-test-[namn]"
